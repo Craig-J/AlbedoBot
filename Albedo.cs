c9812caafd4d28eb407f2b79cbdde3dfb29ba46b
Craig Jeffrey <craigjeffrey3@gmail.com>
2018-04-05 22:54:56 +0100

    First command working

---
+++ Albedo.cs
@@ -0,0 +1,122 @@
+﻿namespace Albedo
+{
+    using Discord;
+    using Discord.Commands;
+    using Discord.WebSocket;
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Albedo
+    {
+        static Albedo()
+        {
+            _token = File.ReadAllText("data/BotToken.txt");
+        }
+
+        private static void Main(string[] args) => new Albedo().MainAsync().GetAwaiter().GetResult();
+
+        private readonly IServiceCollection _map = new ServiceCollection();
+        private readonly CommandService _commands = new CommandService();
+        private IServiceProvider _services;
+        private DiscordSocketClient _client;
+        private static string _token;
+
+        private async Task MainAsync()
+        {
+            var config = new DiscordSocketConfig()
+            {
+                MessageCacheSize = 300
+            };
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            await InitializeCommands();
+
+            _client.Ready += () =>
+            {
+                Console.WriteLine("Bot is ready.");
+                return Task.CompletedTask;
+            };
+
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private static Task Log(LogMessage message)
+        {
+            var cc = Console.ForegroundColor;
+            switch (message.Severity)
+            {
+                case LogSeverity.Critical:
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogSeverity.Verbose:
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message}");
+            Console.ForegroundColor = cc;
+
+            return Task.CompletedTask;
+        }
+
+        private async Task InitializeCommands()
+        {
+            // Repeat this for all the service classes
+            // and other dependencies that your commands might need.
+            //_map.AddSingleton(new SomeServiceClass());
+
+            // When all your required services are in the collection, build the container.
+            // Tip: There's an overload taking in a 'validateScopes' bool to make sure
+            // you haven't made any mistakes in your dependency graph.
+            _services = _map.BuildServiceProvider();
+
+            // Either search the program and add all Module classes that can be found.
+            // Module classes *must* be marked 'public' or they will be ignored.
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+            // Or add Modules manually if you prefer to be a little more explicit:
+            //await _commands.AddModuleAsync<SomeModule>();
+
+            // Subscribe a handler to see if a message invokes a command.
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            // Bail out if it's a System Message.
+            var msg = arg as SocketUserMessage;
+            if (msg == null) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int pos = 0;
+            if (msg.HasStringPrefix("a.", ref pos) || msg.HasMentionPrefix(_client.CurrentUser, ref pos))
+            {
+                // Create a Command Context.
+                var context = new SocketCommandContext(_client, msg);
+
+                // Execute the command. (result does not indicate a return value, 
+                // rather an object stating if the command executed succesfully).
+                var result = await _commands.ExecuteAsync(context, pos, _services);
+
+                // Uncomment the following lines if you want the bot
+                // to send a message if it failed (not advised for most situations).
+                if (!result.IsSuccess)
+                        await msg.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+    }
+}
